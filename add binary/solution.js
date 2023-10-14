e27536040f90c413af818b5af9924a042d2796d8
samndambuki <sam.ndambuki08@gmail.com>
2023-10-14 15:43:24 +0300

    done with addBinary

---
+++ add binary/solution.js
@@ -0,0 +1,25 @@
+function addBinary(a, b) {
+  //parseInt function takes in two arguments
+  //the string to be converted and the base number
+  //string is a and base number is 2 which represents the binary
+
+  //string "11" is equal to 3
+  const aNumber = parseInt(a, 2);
+  const bNumber = parseInt(b, 2);
+
+  //we add the numbers
+  const sum = aNumber + bNumber;
+
+  //toString - converts a number to a binary string
+  //takes in an optional argument which is the base number system to convert to
+  const result = sum.toString(2);
+
+  //return statement exits a function and returns a value to caller
+  return result;
+}
+
+const a = "11";
+const b = "1";
+const result = addBinary(a, b);
+
+console.log(result);
