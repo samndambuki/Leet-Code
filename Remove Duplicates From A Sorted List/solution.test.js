8e0f2e900df4c3d733162aa75ac15f1486c6c0b6
samndambuki <sam.ndambuki08@gmail.com>
2023-11-21 09:04:10 +0300

    added a new test case

--- Remove Duplicates From A Sorted List/solution.test.js
+++ Remove Duplicates From A Sorted List/solution.test.js
@@ -1,9 +1,20 @@
 const { ListNode, deleteDuplicates } = require("./solution");
 
 describe("deleteDuplicates", () => {
+  //test case 1
   test("Removes duplicates from the linked list", () => {
     let head = new ListNode(1, new ListNode(1, new ListNode(2)));
     let result = deleteDuplicates(head);
     expect(result).toEqual(new ListNode(1, new ListNode(2)));
+  });
+
+  //test case 2
+  test("Remove duplicates from the linked list", () => {
+    let head = new ListNode(
+      1,
+      new ListNode(1, new ListNode(2, new ListNode(3)))
+    );
+    let result = deleteDuplicates(head);
+    expect(result).toEqual(new ListNode(1, new ListNode(2, new ListNode(3))));
   });
 });
